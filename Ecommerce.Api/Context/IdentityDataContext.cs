736a29ce5828cb6ed2858cd4b9cb9f6bf5593e27
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-02-26 21:58:45 -0300

    fix: IdentityDataContext

--- Ecommerce.Api/Context/IdentityDataContext.cs
+++ Ecommerce.Api/Context/IdentityDataContext.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-namespace Ecommerce.Api.Context
-{
-    public class IdentityDataContext
+namespace Ecommerce.Api.Context;
+
+    public class IdentityDataContext : IdentityDbContext
     {
-        
+        public IdentityDataContext(DbContextOptions<IdentityDataContext> options) : base(options) { }
     }
-}
