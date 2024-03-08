08564fdf34585a544f4893a969cb1eb34bc5c8b9
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-08 16:42:00 -0300

    Update Product.cs

--- Ecommerce.Api/Entities/Product.cs
+++ Ecommerce.Api/Entities/Product.cs
@@ -2,21 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Ecommerce.Api.Entities.Shared;
 
 namespace Ecommerce.Api.Entities;
 
-    public class Product : Entity
-    {
-        public string Name { get; set;}
-        public double price {get;set;}
-        public string imageUrl {get;set;}
+public class Product : Entity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public string ImageUrl { get; set; }
+    public string Unit { get; set; }
+    public int Quantity { get; set; }
+    public string Tags { get; set; }
+    public string CategoryId { get; set; }
+    public Category? Category { get; set; }
 
 
-    public Product(string name, double price, string imageUrl)
-    {
-        Name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
 }
