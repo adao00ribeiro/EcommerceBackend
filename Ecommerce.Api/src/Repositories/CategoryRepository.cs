48ac3b7cfd2a02cb7de5f940a02dd798df386015
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-09 10:46:09 -0300

    feat: category

---
+++ Ecommerce.Api/src/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Api.src.Context;
+using Ecommerce.Api.src.Entities;
+using Ecommerce.Api.src.Interfaces.Repositories;
+using Ecommerce.Api.src.Repositories.Shared;
+
+namespace Ecommerce.Api.src.Repositories;
+
+public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+{
+    public CategoryRepository(DataContext dataContext) : base(dataContext)
+    {
+    }
+}
