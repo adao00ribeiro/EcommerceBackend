46c85bd975655500a7dec5c6a6badac225c99a33
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-11 15:06:52 -0300

    feat: CartController

---
+++ Ecommerce.Api/src/Controllers/CartController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Api.src.Controllers.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Api.src.Controllers;
+
+
+[ApiVersion("1.0")]
+public class CartController : ApiControllerBase
+{
+
+}
