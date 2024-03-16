984034c3fd9c0d411afa5f60bc3ae45f04b43c97
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-15 22:13:32 -0300

    fix : productDto

--- Ecommerce.Api/src/DTOs/ProductDto.cs
+++ Ecommerce.Api/src/DTOs/ProductDto.cs
@@ -8,9 +8,29 @@ namespace Ecommerce.Api.src.DTOs;
 
 public record ProductDto
 {
+    public string Id { get;  set; }
+    public string Name { get;  set; }
+    public string Description { get;  set; }
+    public double Price { get;  set; }
+    public string ImageUrl { get;  set; }
+    public string Unit { get;  set; }
+    public int Quantity { get;  set; }
+    public string Tags { get;  set; }
+    public string CategoryId { get;  set; }
+    public Category? Category { get;  set; }
     internal static ProductDto ConvertToDto(Product product)
     {
-        return new ProductDto();
+        return new ProductDto(){
+        Name = product.Name,
+        Description = product.Description,
+        Price = product.Price,
+        ImageUrl = product.ImageUrl,
+        Unit = product.Unit,
+        Quantity = product.Quantity,
+        Tags = product.Tags,
+        CategoryId = product.CategoryId,
+        Category = product.Category,
+        };
     }
 
     internal static Product ConvertToEntity(ProductDto product)
