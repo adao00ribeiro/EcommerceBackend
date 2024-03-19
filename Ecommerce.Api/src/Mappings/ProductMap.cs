1490ca5c72e4bfac53ab6d63fb6c9b03342a4340
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-19 17:50:14 -0300

    Update ProductMap.cs

--- Ecommerce.Api/src/Mappings/ProductMap.cs
+++ Ecommerce.Api/src/Mappings/ProductMap.cs
@@ -8,7 +8,46 @@ public class ProductMap : IEntityTypeConfiguration<Product>
 {
     public void Configure(EntityTypeBuilder<Product> builder)
     {
-        
-        
-    }   
+        builder
+        .ToTable("products");
+        builder
+        .HasKey(x => x.Id);
+        builder
+        .Property(x => x.Id)
+        .ValueGeneratedOnAdd();
+        builder
+        .Property(x => x.Name)
+        .HasMaxLength(255)
+        .IsRequired();
+        builder
+        .Property(x => x.Description)
+        .HasMaxLength(255)
+        .IsRequired();
+        builder
+        .Property(x => x.Price);
+        builder
+        .Property(x => x.ImageUrl)
+        .HasMaxLength(255)
+        .IsRequired();
+        builder
+        .Property(x => x.Unit)
+        .HasMaxLength(255)
+        .IsRequired();
+        builder
+        .Property(x => x.Quantity);
+        builder
+        .Property(x => x.Tags)
+        .HasMaxLength(255)
+        .IsRequired();
+        builder
+        .Property(x => x.CategoryId)
+        .HasMaxLength(255)
+        .IsRequired();
+        builder
+        .HasOne(x => x.Category)
+        .WithMany()
+        .HasForeignKey(x => x.CategoryId)
+        .IsRequired();
+
+    }
 }
