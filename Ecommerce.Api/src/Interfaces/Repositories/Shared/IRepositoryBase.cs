957d9cb7afab43c7027036fac663249c312fabfe
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-19 17:50:30 -0300

    Update IRepositoryBase.cs

--- Ecommerce.Api/src/Interfaces/Repositories/Shared/IRepositoryBase.cs
+++ Ecommerce.Api/src/Interfaces/Repositories/Shared/IRepositoryBase.cs
@@ -1,10 +1,11 @@
+using System.Linq.Expressions;
 using Ecommerce.Api.src.Entities.Shared;
 
 namespace Ecommerce.Api.src.Interfaces.Repositories.Shared;
 
 public interface IRepositoryBase<TEntity> : IDisposable where TEntity : Entity
 {
-    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<IEnumerable<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includeProperties);
     Task<TEntity?> GetByIdAsync(string id);
     Task<string> AddAsync(TEntity objeto);
     Task UpdateAsync(TEntity objeto);
