882a32f1467f617e018b28a2bcba79ac300aeb12
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-19 17:50:26 -0300

    Update CategoryRepository.cs

--- Ecommerce.Api/src/Repositories/CategoryRepository.cs
+++ Ecommerce.Api/src/Repositories/CategoryRepository.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Ecommerce.Api.src.Context;
-using Ecommerce.Api.src.DTOs;
 using Ecommerce.Api.src.Entities;
 using Ecommerce.Api.src.Interfaces.Repositories;
 using Ecommerce.Api.src.Repositories.Shared;
-using Microsoft.EntityFrameworkCore;
+
 
 namespace Ecommerce.Api.src.Repositories;
 
@@ -17,5 +12,5 @@ public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
     {
     }
 
-    
+
 }
