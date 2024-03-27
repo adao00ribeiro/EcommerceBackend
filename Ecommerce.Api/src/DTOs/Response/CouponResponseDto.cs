594399aa2f1655a61451a0e6ddad1d0c51ab751c
Adão Ribeiro <adao-eduardo@hotmail.com>
2024-03-26 22:28:03 -0300

    fix: couponRepository , dtos

--- Ecommerce.Api/src/DTOs/Response/CouponResponseDto.cs
+++ Ecommerce.Api/src/DTOs/Response/CouponResponseDto.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Ecommerce.Api.src.Entities;
 
 namespace Ecommerce.Api.src.DTOs.Response;
 
-public class CouponResponseDto
+public record CouponResponseDto(string CouponCode , decimal DiscountAmount)
 {
     internal static CouponResponseDto ConvertToDto(Coupon coupon)
     {
-        return new CouponResponseDto();
+        return new CouponResponseDto(coupon.CouponCode , coupon.DiscountAmount);
     }
 }
--- Ecommerce.Api/src/Repositories/CouponRepository.cs
+++ Ecommerce.Api/src/Repositories/CouponRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Ecommerce.Api.src.Context;
 using Ecommerce.Api.src.Entities;
 using Ecommerce.Api.src.Interfaces.Repositories;
